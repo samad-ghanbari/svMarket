19454ddbc3dc89245e626d1a7739321cf531cdf9
Samad Ghanbari <Ghanbari.Samad@gmail.com>
2025-10-29 12:42:03 +0330

    app port number and fonts done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>تست آزمایشی فونت</h1>
+<h1 class="font-yekan text-4xl">تست آزمایشی فونت</h1>
+<h1 class="font-yekan-bold text-4xl">تست آزمایشی فونت</h1>
+<p class="font-phamelo text-4xl">
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
+<p class="font-phamelo-bold text-4xl">
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
