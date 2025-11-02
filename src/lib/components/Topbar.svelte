6181966e59e4ce0a32621f209c5038bf1dd33c48
Samad Ghanbari <Ghanbari.Samad@gmail.com>
2025-11-02 17:14:52 +0330

    tobar updated

--- src/lib/components/Topbar.svelte
+++ src/lib/components/Topbar.svelte
@@ -1,31 +1,117 @@
 <script lang="ts">
-	import { Search, GradientButton, Tooltip } from 'flowbite-svelte';
-	import { UserIcon, ChatBubbleLeftRightIcon, ShoppingCartIcon } from 'heroicons-svelte/24/solid';
+	import {
+		Search,
+		GradientButton,
+		Button,
+		Tooltip,
+		Popover,
+		Avatar,
+		Listgroup,
+		ListgroupItem,
+		Hr
+	} from 'flowbite-svelte';
+	import {
+		UserIcon,
+		ChatBubbleLeftRightIcon,
+		ShoppingCartIcon,
+		ChevronDownIcon,
+		Cog6ToothIcon,
+		ShoppingBagIcon,
+		HeartIcon,
+		MapPinIcon,
+		ArrowLeftOnRectangleIcon
+	} from 'heroicons-svelte/24/solid';
+
 	function handleSearch() {
 		console.log('searching');
 	}
 
-	let shoppingCartRef;
+	function handleSignOut() {
+		console.log('log out');
+	}
 </script>
 
 <div
-	class="relative z-10 flex h-[50px] w-full items-center justify-end bg-linear-to-r from-blue-50 to-green-50"
+	class="relative z-10 flex h-[50px] w-full items-center justify-end bg-linear-60 from-blue-50 to-neutral-100"
 >
 	<div class="mr-auto ml-4 flex h-10 w-[140px] items-center justify-start">
-		<a class=" h-8 w-8" href="/">
-			<ChatBubbleLeftRightIcon class="h-8 w-8 text-blue-300 hover:text-blue-400" />
-			<Tooltip type="light" placement="bottom">پیام‌های دریافتی</Tooltip>
-		</a>
+		<button id="messagesBtn" class=" h-6 w-6 border-0 bg-transparent">
+			<ChatBubbleLeftRightIcon class="h-6 w-6 text-neutral-400 hover:text-neutral-500" />
+		</button>
+		<Tooltip
+			class="bg-neutral-100 text-gray-500"
+			triggeredBy="#messagesBtn"
+			type="light"
+			placement="bottom">پیام‌های دریافتی</Tooltip
+		>
 
-		<a class=" hidden h-8 w-8 md:block" href="/">
-			<UserIcon class="h-8 w-8 text-blue-300 hover:text-blue-400" />
-			<Tooltip type="light" placement="bottom">پروفایل کاربر</Tooltip>
+		<a id="shoppingBtn" class=" mx-4 hidden h-6 w-6 md:block" href="/">
+			<ShoppingCartIcon class="h-6 w-6 text-neutral-400 hover:text-neutral-500" />
 		</a>
+		<Tooltip
+			triggeredBy="#shoppingBtn"
+			class="bg-neutral-100 text-gray-500"
+			type="light"
+			placement="bottom">سبد خرید</Tooltip
+		>
 
-		<a bind:this={shoppingCartRef} class="hidden h-8 w-8 md:block" href="/">
-			<ShoppingCartIcon class="h-8 w-8 text-blue-300 hover:text-blue-400" />
-			<Tooltip type="light" placement="bottom">سبد خرید</Tooltip>
-		</a>
+		<button id="profileBtn" class=" hidden h-6 w-6 border-0 bg-transparent md:block">
+			<div class="flex flex-col text-neutral-400 hover:text-neutral-500">
+				<UserIcon class="h-6 w-6 " />
+				<ChevronDownIcon class="w4 -mt-1 h-4 " />
+			</div>
+		</button>
+		<Tooltip
+			triggeredBy="#profileBtn"
+			class="bg-neutral-100 text-gray-500"
+			type="light"
+			placement="bottom">پروفایل کاربر</Tooltip
+		>
+		<Popover triggeredBy="#profileBtn" trigger="click" class="bg-neutral-100">
+			<div class="mb-1 h-auto w-52">
+				<Listgroup active class="w-full">
+					<ListgroupItem
+						class="h-14 justify-end hover:bg-blue-100 hover:text-black active:text-black active:ring-0"
+						href="/"
+					>
+						صمد قنبری
+						<Avatar src="/assets/images/user.png" alt="صمد" />
+					</ListgroupItem>
+
+					<ListgroupItem
+						class="justify-end hover:text-black active:text-black active:ring-0"
+						href="/"
+					>
+						سفارشات
+						<ShoppingBagIcon class="mr-2 inline h-5 w-5 text-right text-gray-500" />
+					</ListgroupItem>
+
+					<ListgroupItem
+						class="justify-end hover:text-black active:text-black active:ring-0"
+						href="/"
+						>لیست علایق
+						<HeartIcon class="mr-2 inline h-5 w-5 text-right text-red-500" />
+					</ListgroupItem>
+
+					<ListgroupItem
+						class="justify-end hover:text-black active:text-black active:ring-0"
+						href="/"
+					>
+						آدرس‌ها
+						<MapPinIcon class="mr-2 inline h-5 w-5 text-right text-blue-400" />
+					</ListgroupItem>
+
+					<ListgroupItem
+						class="justify-end hover:bg-red-500 hover:text-white active:text-black active:ring-0"
+						onclick={handleSignOut}
+					>
+						خروج از حساب کاربری
+
+						<ArrowLeftOnRectangleIcon class="mr-2 inline h-5 w-5 text-right" />
+					</ListgroupItem>
+				</Listgroup>
+			</div>
+		</Popover>
 	</div>
 
 	<div class="mx-1 w-full max-w-4xl" dir="rtl">
