b071d5fc306a89fefda36341cc23b86945c5d044
Samad Ghanbari <Ghanbari.Samad@gmail.com>
2025-11-03 14:51:51 +0330

    navbar init

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,100 +1,45 @@
 <script lang="ts">
-	import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, MegaMenu, Button } from 'flowbite-svelte';
-	import { ChevronDownIcon } from 'heroicons-svelte/24/solid';
-	let cats = [
-		{ name: 'خواروبار', href: '/groc' },
-		{ name: 'خشکبار', href: '/nuts' },
-		{ name: 'نوشیدنی‌ها', href: '/drinks' },
-		{ name: 'روغن', href: '/oil' },
-		{ name: 'بهداشتی و نظافتی', href: '/oil' },
-		{ name: 'قهوه', href: '/oil' },
-		{ name: 'حبوبات', href: '/oil' },
-		{ name: 'ادویه', href: '/oil' },
-		{ name: 'تنقلات', href: '/oil' },
-		{ name: 'چای و دمنوش', href: '/oil' }
-	];
-	let stores = [
-		{ name: 'فروشگاه مجیدیه', href: '/groc' },
-		{ name: 'فروشگاه لویزان', href: '/nuts' },
-		{ name: 'فوشگاه ونک', href: '/drinks' },
-		{ name: 'فروشگاه چیتگر', href: '/oil' },
-		{ name: 'فروشگاه صادقیه', href: '/oil' },
-		{ name: 'فروشگاه آزادی', href: '/oil' },
-		{ name: 'فروشگاه پیروزی', href: '/oil' },
-		{ name: 'فروشگاه شهرک غرب', href: '/oil' }
-	];
+	import {
+		ChevronDownIcon,
+		HomeIcon,
+		BuildingStorefrontIcon,
+		Square3Stack3dIcon
+	} from 'heroicons-svelte/24/solid';
+
+	import { Popover } from 'flowbite-svelte';
 </script>
 
-<Navbar>
-	<NavBrand href="/">
-		<img src="/assets/images/logo/logo128.png" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-		<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Market</span>
-	</NavBrand>
-	<NavHamburger />
-	<NavUl>
-		<NavLi href="/about">درباره ما</NavLi>
-		<NavLi href="/contacts">تماس با ما</NavLi>
-		<NavLi href="/services">خدمات</NavLi>
-		<NavLi href="/products">محصولات</NavLi>
+<div class="relative hidden h-10 w-full bg-transparent px-2 py-2 md:block">
+	<div class="justify-betwee mx-auto flex h-full w-full flex-wrap items-center">
+		<!-- location -->
 
-		<NavLi class="cursor-pointer">
-			فروشگاه‌ها و شعبه‌ها<ChevronDownIcon
-				class="ms-2 inline h-6 w-6 text-primary-800 dark:text-white"
-			/>
-		</NavLi>
-		<MegaMenu full items={stores} class="text-right">
-			{#snippet extra()}
-				<a
-					href="/"
-					class="mx-10 mt-4 block rounded-lg bg-gray-500 bg-cover bg-local bg-center bg-no-repeat p-4 text-right bg-blend-multiply hover:bg-blend-soft-light dark:hover:bg-blend-darken"
-					style="background-image: url(/assets/images/logo/logo128.png)"
+		<ul class="mr-4 ml-auto flex flex-row items-center justify-end gap-4 text-base">
+			<li class="text-gray-500 hover:text-gray-800">
+				<button id="storesLink" class="inline-flex items-center gap-1 border-0 bg-transparent">
+					<ChevronDownIcon class="h-3 w-3" />
+					فروشگاه‌ها و شعبه‌ها
+					<BuildingStorefrontIcon class="h-4 w-4 " />
+				</button>
+				<Popover triggeredBy="#storesLink" trigger="click"
+					><div class="h-[500px] w-[400px] bg-neutral-100"></div></Popover
 				>
-					<p
-						class="mb-5 max-w-xl p-0 text-right text-sm leading-tight font-extrabold tracking-tight text-white"
-					>
-						فروشگاه‌ها و شعبه‌ها
-					</p>
-				</a>
-			{/snippet}
-			{#snippet children({ item })}
-				<a
-					href={item.href}
-					class="bg-green-100 text-right hover:text-primary-600 hover:underline dark:hover:text-primary-500"
+			</li>
+			<li class="text-gray-500 hover:text-gray-800">
+				<button id="catsLink" class="inline-flex items-center gap-1 border-0 bg-transparent">
+					<ChevronDownIcon class="h-3 w-3" />
+					دسته‌بندی کالا
+					<Square3Stack3dIcon class="h-4 w-4 " />
+				</button>
+				<Popover triggeredBy="#catsLink" trigger="click"
+					><div class="h-[calc(100dvh-200px)] w-[calc(100dvw-200px)] bg-neutral-100"></div></Popover
 				>
-					{item.name}
+			</li>
+			<li class="text-gray-500 hover:text-gray-800">
+				<a class="inline-flex items-center gap-1" href="/">
+					صفحه اصلی
+					<HomeIcon class="h-4 w-4 " />
 				</a>
-			{/snippet}
-		</MegaMenu>
-
-		<NavLi class="cursor-pointer">
-			دسته‌بندی‌ کالاها<ChevronDownIcon
-				class="ms-2 inline h-6 w-6 text-primary-800 dark:text-white"
-			/>
-		</NavLi>
-		<MegaMenu full items={cats} class="mx-10 text-right">
-			{#snippet children({ item })}
-				<a
-					href={item.href}
-					class="bg-green-100 text-right hover:text-primary-600 hover:underline dark:hover:text-primary-500"
-				>
-					{item.name}
-				</a>
-			{/snippet}
-			{#snippet extra()}
-				<a
-					href="/"
-					class="mx-10 mt-4 block rounded-lg bg-gray-500 bg-cover bg-local bg-center bg-no-repeat p-4 text-right bg-blend-multiply hover:bg-blend-soft-light dark:hover:bg-blend-darken"
-					style="background-image: url(/assets/images/logo/logo128.png)"
-				>
-					<p
-						class="mb-5 max-w-xl p-0 text-right text-sm leading-tight font-extrabold tracking-tight text-white"
-					>
-						دسته بندی کالاها
-					</p>
-				</a>
-			{/snippet}
-		</MegaMenu>
-
-		<NavLi href="/">صفحه اصلی</NavLi>
-	</NavUl>
-</Navbar>
+			</li>
+		</ul>
+	</div>
+</div>
