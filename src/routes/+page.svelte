6872144192f5ad19514ab8e930fb6d22ba20ed79
Samad Ghanbari <Ghanbari.Samad@gmail.com>
2025-11-08 07:12:54 +0330

    sec

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { Alert } from 'flowbite-svelte';
-	import { HomeIcon as HomeSolid } from 'heroicons-svelte/24/solid';
-	import { HomeIcon as HomeOutline } from 'heroicons-svelte/24/outline';
-
-	//import { PUBLIC_value } from '$env/static/public';
-	//import { env } from '$env/dynamic/public';
-</script>
