f25b4535fa1fa093de5a37285036c4969790e7d9
Samad Ghanbari <Ghanbari.Samad@gmail.com>
2025-11-15 16:54:40 +0330

    favorits search api

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
+import path from 'path';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$types: path.resolve('./src/lib/types'),
+			$backend: path.resolve('./backend'),
+			$root: path.resolve('./'),
+			$lib: path.resolve('./src/lib/'),
+			$src: path.resolve('./src')
+		}
 	}
 };
 
